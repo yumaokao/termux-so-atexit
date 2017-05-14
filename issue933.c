6b140d2adceea069208eeb0bd0e68433842abfe8
yumaokao <ymkq9h@gmail.com>
2017-05-14 08:33:44 +0800

    add atexit called in main

--- issue933.c
+++ issue933.c
@@ -1,3 +1,15 @@
 #define GPGRT_ENABLE_ES_MACROS
 #include <gpg-error.h>
-int main() { es_putc('A', es_stdout); exit(0);}
+/* #include <stdio.h>
+#include <stdlib.h>
+
+void main_atexit(void) {
+    printf("atexit called from main\n");
+} */
+
+int main() {
+    /* printf("atexit(main_atexit)\n");
+    atexit(main_atexit); */
+    es_putc('A', es_stdout);
+    exit(0);
+}
